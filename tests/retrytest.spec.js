aa1fadcbe3c2f8aa54aeead5b19afe27d363b5a2
Datta@1212 <dattacheke@gmail.com>
2026-06-21 20:25:53 +0530

    clean Playwright tests - remove secrets

--- tests/retrytest.spec.js
+++ tests/retrytest.spec.js
@@ -1,4 +1,4 @@
-const { test, expect, config } = require ('@playwright/test');
+const { test, expect, config } = require('@playwright/test');
 
 test('retry test', async () => {
   expect(1).toBe(2);
